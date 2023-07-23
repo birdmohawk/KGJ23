0fe153d59967d64ea515c134e8967e5c50eeb1fe
bellsye <bellsyeillman@gmail.com>
2023-07-23 12:35:57 +1200

    created main menu

---
+++ Assets/UI/OnButtonPressScript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OnButtonPressScript : MonoBehaviour
+{
+    //public GameObject instructions;
+    public AudioSource buttonSound;
+    //public Scene scene;
+
+    /*void Start()
+    {
+        instructions.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape") && !instructions) //esc key assigned in input manager
+            instructions.SetActive(true);
+
+        else
+            instructions.SetActive(false);
+    }*/
+    public void OnButtonPressPlay() //play game
+    {
+        SceneManager.LoadScene("prototype");
+    }
+
+    public void OnButtonPressQuit() //quit game
+    {
+        Application.Quit();
+    }
+
+    /*public void OnButtonNextLvlTwo() //quit game
+    {
+        SceneManager.LoadScene("Level_Two");
+    }
+
+    public void OnButtonPressNxtLvlThree()
+    {
+        SceneManager.LoadScene("Level_Three");
+    }
+
+    public void ResetScene() //play game
+    {
+        SceneManager.LoadScene("Level_One");
+    }
+
+    public void ButtonSound() //play game
+    {
+        buttonSound.Play();
+    }*/
+}
